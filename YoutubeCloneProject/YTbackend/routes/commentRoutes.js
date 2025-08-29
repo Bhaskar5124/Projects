99f943725b3868bad9447d52af6882057ad4360a
Bhaskar5124 <bhaskarchourasia5124@gmail.com>
2025-08-29 17:41:56 +0530

    Delete YoutubeCloneProject directory
    Uploaded all project instead of one

--- YoutubeCloneProject/YTbackend/routes/commentRoutes.js
+++
@@ -1,6 +0,0 @@
-import { displayAllComments, newComment } from "../controllers/commentController.js";
-
-export function commentsRoutes(app){
-    app.post('/newcomment', newComment);
-    app.get('/comments', displayAllComments);
-}
